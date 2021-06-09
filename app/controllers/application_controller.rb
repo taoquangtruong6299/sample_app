7f6da65e14f1666692485f555880eab21195ffe6
penpao <taoquangtruong621999@gmail.com>
2021-06-09 15:14:19 +0700

    Init project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-    def hello
-        render html: "hello, world!"
-        end
+  before_action :set_locale
+  def hello
+    render html: "hello, world!"
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+  def default_url_options
+    {locale: I18n.locale}
+  end
 end
